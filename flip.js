8d57e0a1c954939baaba6d68f11fb1f2922e7827
Daymaul <devonbordonaro@yahoo.com>
2019-11-25 23:39:40 -0500

    what is happening

--- flip.js
+++ flip.js
@@ -1,4 +1,12 @@
-var image = document.querySelector('.pic');
-image.addEventListener( 'click', function() {
-  image.classList.toggle('is-flipped');
+var image1 = document.querySelector('.pic1');
+var image2 = document.querySelector('.pic2');
+var image3 = document.querySelector('.pic3');
+image1.addEventListener( 'click', function() {
+  image1.classList.toggle('is-flipped');
+});
+image2.addEventListener( 'click', function() {
+  image2.classList.toggle('is-flipped');
+});
+image3.addEventListener( 'click', function() {
+  image3.classList.toggle('is-flipped');
 });
